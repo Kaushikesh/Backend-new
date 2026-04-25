15babe9450f7c7198f242218b0891f6358ddb214
Kaushikesh.S IT24100689 <it24100689@my.sliit.lk>
2026-04-25 19:25:58 +0530

    first commit

---
+++ models/Item.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const ItemSchema = new mongoose.Schema({
+ name: { type: String, required: true },
+ description: { type: String, required: true },
+ // TODO: Add price field here
+}, { timestamps: true });
+module.exports = mongoose.model('Item', ItemSchema)
